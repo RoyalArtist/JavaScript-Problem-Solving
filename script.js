c3cae56a710d19edcc1278914a5b90bb3c57bf3d
Norbert <106980094+RoyalArtist@users.noreply.github.com>
2023-04-17 17:46:29 +0200

    Add JavaScript solution to find which kid can have the greatest number of candies after extra candies are given
    This repository contains a JavaScript solution for a coding challenge that involves finding which kid can have the greatest number of candies after extra candies are given. The solution is implemented in a function called 'kidsWithCandies', which takes an array of integers representing the number of candies each kid has and an extra number of candies as input. The function returns a boolean array of the same length as the input array, indicating whether each kid can have the greatest number of candies after the extra candies are given.
    
    The 'kidsWithCandies' function works by finding the maximum number of candies in the input array, then iterating over the array to check if adding the extra candies to each kid's candies will result in them having the greatest number of candies. The function returns an array of boolean values indicating the result for each kid.
    
    This solution can be used as a reference for other coding challenges involving array manipulation and conditional statements in JavaScript.

---
+++ script.js
@@ -0,0 +1,23 @@
+    //Challenge 1
+
+// There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+//Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+//Note that multiple kids can have the greatest number of candies.
+
+function kidsWithCandies(candies, extraCandies) {
+    const maxCandies = Math.max(...candies)
+    const result = []
+
+    for(let i = 0; i < candies.length; i++) {
+        if(candies[i] + extraCandies >= maxCandies)  {
+            result.push(true)
+        } else {
+            result.push(false)
+        }
+    }
+
+    return result
+}
+
+// This function first finds the maximum number of candies in the array. Then it loops through the array and checks if the current kid has the maximum number of candies. If so, it pushes true to the result array. If not, it pushes false to the result array.
+// finally, it returns the result array.
