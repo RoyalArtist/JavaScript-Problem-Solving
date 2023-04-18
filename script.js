6b5047a1d5f5963ef93efce80e75ddba864ec4dc
Norbert <106980094+RoyalArtist@users.noreply.github.com>
2023-04-18 14:54:10 +0200

    Challenge 2: Counter Function
    The Counter Function challenge is about creating a function that takes an integer n and returns another function that acts as a counter.
    
    The returned function should initially return n and then return 1 more than the previous value every subsequent time it is called. For example, if n is 10, then the first call to the returned function should return 10, the second call should return 11, and so on.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
     //Challenge 1
 
-// There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+//There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
 //Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
 //Note that multiple kids can have the greatest number of candies.
 
@@ -25,3 +25,23 @@ console.log(result(candies, extraCandies))
 
 // This function first finds the maximum number of candies in the array. Then it loops through the array and checks if the current kid has the maximum number of candies. If so, it pushes true to the result array. If not, it pushes false to the result array.
 // finally, it returns the result array.
+
+    //Challenge 2
+
+// Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+    const counter = createCounter(5)
+
+    function createCounter(n) {
+        return function() {
+            return n++
+        }
+    }
+
+    // This function takes an integer n as an argument and returns another function that acts as a counter.
+    // The returned function keeps track of the current count by using a closure to store the current value of n.
+    // Each time the function is called, it increments n by 1 and returns the new value.
+
+    console.log(counter()) // 5
+    console.log(counter()) // 6
+    console.log(counter()) // 7
